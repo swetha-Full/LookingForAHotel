be995f24695ccc5b1637cf859a10f9ce9e996d4f
swetha-Full <swetha.shankar@anywhere.co>
2018-02-09 19:27:31 +0530

    forgot to save

--- src/My_Task/hotel/Hotel.java
+++ src/My_Task/hotel/Hotel.java
@@ -1,10 +1,30 @@
 package My_Task.hotel;
 
-public class Hotel {
+import java.io.IOException;
 
+import hotel.hotelBooking;
+
+public class Hotel {
+	String name;
+	int ratings;
+	double costPerDay;
+	int discountPercentage;
+	int discountDays;
+	public Hotel(String hotelName, int rating, double cost, int offer,int validOffer)throws IOException {
+		this.name = hotelName;
+		this.ratings = rating;
+		this.costPerDay= cost;
+		this.discountPercentage=offer;
+		this.discountDays=validOffer;
+		public static String hotelBooking()
+		{
+			
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Hotel hotels[] = new Hotel[numberOfHotels];
 	}
 
 }
